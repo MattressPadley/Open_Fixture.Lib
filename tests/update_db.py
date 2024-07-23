c9a0d8f08e4d1f5a5837ffe76961c96f243ab407
Matt Hadley <hello@matthadley.me>
2024-07-23 13:09:13 -0700

    update script

--- tests/update_db.py
+++ tests/update_db.py
@@ -3,46 +3,54 @@ import sys
 import json
 from pymongo import MongoClient
 from bson import ObjectId
-from dotenv import load_dotenv
 
-load_dotenv()
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
 
 # Connect to the MongoDB database
 client = MongoClient(os.getenv("MONGO_URI"))
-db = client['filmset-lighting-library']
+db = client["filmset-lighting-library"]
+
 
 def update_entry(data, data_type):
 
     collection = db[data_type]
-    name = data['Name']
+    name = data["Name"]
     # Find the entry in the collection based on the pull request number
-    entry = collection.find_one({'Name': name})
+    entry = collection.find_one({"Name": name})
 
     if entry:
         # Convert the id to ObjectId
-        data['_id'] = ObjectId(data['_id'])
+        data["_id"] = ObjectId(data["_id"])
         # Update the entry with the new data
-        collection.update_one({'_id': entry['_id']}, {'$set': data})
-        print(f"Entry for {name} updated successfully.")
+        collection.update_one({"_id": entry["_id"]}, {"$set": data})
+
+        return f"Entry for {name} updated successfully."
+
     else:
         # Create a new entry with the provided data
         collection.insert_one(data)
-        print(f"New entry for {name} created successfully.")
-        new_entry_id = collection.find_one({'Name': name})['_id']
-        print(f"ID of the new entry: {new_entry_id}")
+        new_entry_id = collection.find_one({"Name": name})["_id"]
         add_id(path, new_entry_id)
 
+        return f"Entry for {name} created successfully."
+
+
 def add_id(path, id):
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         data = json.load(file)
-    data['_id'] = str(id)
-    with open(path, 'w') as file:
+    data["_id"] = str(id)
+    with open(path, "w") as file:
         json.dump(data, file, indent=4)
-                  
+
+
 def get_data(path):
     with open(path) as file:
         json_data = json.load(file)
     return json_data
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -53,5 +61,7 @@ if __name__ == "__main__":
     data_type = sys.argv[2]
 
     data = get_data(path)
-    update_entry(data, data_type)
-    
+    response = update_entry(data, data_type)
+
+    with open("update_output.md", "a") as f:
+        f.write(f"{response}\n")
